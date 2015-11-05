37caa7c45078b3e6cf5ff363d035e9e30f213fc6
stephperez09 <Stephanie.Perez01@student.csulb.edu>
2015-11-05 12:17:42 -0800

    initial version

---
+++ CloudLion/src/main/java/cloudlion/controllers/TextController.java
@@ -0,0 +1,23 @@
+package cloudlion.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cloudlion.texteditor.TextEditor;
+
+@RestController
+@RequestMapping("TextEditor")
+public class TextController {
+	
+	@Autowired
+	private TextEditor texteditor;
+	
+	@RequestMapping("texteditor")
+	public static void main(String[] args) {
+		new TextEditor();
+	}
+	
+	
+
+}
