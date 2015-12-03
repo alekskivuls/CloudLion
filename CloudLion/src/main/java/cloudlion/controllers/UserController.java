e06f27080c02fb43d6bf8ec6b0fb0be6fcee3c95
Aleks Kivuls <akivuls@yahoo.com>
2015-12-03 02:01:51 -0800

    Fix signup page and remove pac4j template pages

--- CloudLion/src/main/java/cloudlion/controllers/UserController.java
+++ CloudLion/src/main/java/cloudlion/controllers/UserController.java
@@ -20,55 +20,58 @@ import java.util.NoSuchElementException;
 @Controller
 public class UserController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-    private final UserService userService;
-    private final UserCreateFormValidator userCreateFormValidator;
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+	private final UserService userService;
+	private final UserCreateFormValidator userCreateFormValidator;
 
-    @Autowired
-    public UserController(UserService userService, UserCreateFormValidator userCreateFormValidator) {
-        this.userService = userService;
-        this.userCreateFormValidator = userCreateFormValidator;
-    }
+	@Autowired
+	public UserController(UserService userService, UserCreateFormValidator userCreateFormValidator) {
+		this.userService = userService;
+		this.userCreateFormValidator = userCreateFormValidator;
+	}
 
-    @InitBinder("form")
-    public void initBinder(WebDataBinder binder) {
-        binder.addValidators(userCreateFormValidator);
-    }
+	@InitBinder("form")
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(userCreateFormValidator);
+	}
 
-    @PreAuthorize("@currentUserServiceImpl.canAccessUser(principal, #id)")
-    @RequestMapping("/user/{id}")
-    public ModelAndView getUserPage(@PathVariable Long id) {
-        LOGGER.debug("Getting user page for user={}", id);
-        return new ModelAndView("user", "user", userService.getUserById(id)
-                .orElseThrow(() -> new NoSuchElementException(String.format("User=%s not found", id))));
-    }
+	@PreAuthorize("@currentUserServiceImpl.canAccessUser(principal, #id)")
+	@RequestMapping("/user/{id}")
+	public ModelAndView getUserPage(@PathVariable Long id) {
+		LOGGER.debug("Getting user page for user={}", id);
+		return new ModelAndView("user", "user", userService.getUserById(id)
+				.orElseThrow(() -> new NoSuchElementException(String.format("User=%s not found", id))));
+	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/user/create", method = RequestMethod.GET)
-    public ModelAndView getUserCreatePage() {
-        LOGGER.debug("Getting user create form");
-        return new ModelAndView("user_create", "form", new UserCreateForm());
-    }
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/user/create", method = RequestMethod.GET)
+	public ModelAndView getUserCreatePage() {
+		LOGGER.debug("Getting user create form");
+		return new ModelAndView("user_create", "form", new UserCreateForm());
+	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/user/create", method = RequestMethod.POST)
-    public String handleUserCreateForm(@Valid @ModelAttribute("form") UserCreateForm form, BindingResult bindingResult) {
-        LOGGER.debug("Processing user create form={}, bindingResult={}", form, bindingResult);
-        if (bindingResult.hasErrors()) {
-            // failed validation
-            return "user_create";
-        }
-        try {
-            userService.create(form);
-        } catch (DataIntegrityViolationException e) {
-            // probably email already exists - very rare case when multiple admins are adding same user
-            // at the same time and form validation has passed for more than one of them.
-            LOGGER.warn("Exception occurred when trying to save the user, assuming duplicate email", e);
-            bindingResult.reject("email.exists", "Email already exists");
-            return "user_create";
-        }
-        // ok, redirect
-        return "redirect:/users";
-    }
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/user/create", method = RequestMethod.POST)
+	public String handleUserCreateForm(@Valid @ModelAttribute("form") UserCreateForm form,
+			BindingResult bindingResult) {
+		LOGGER.debug("Processing user create form={}, bindingResult={}", form, bindingResult);
+		if (bindingResult.hasErrors()) {
+			// failed validation
+			return "redirect:/signup";
+		}
+		try {
+			userService.create(form);
+		} catch (DataIntegrityViolationException e) {
+			// probably email already exists - very rare case when multiple
+			// admins are adding same user
+			// at the same time and form validation has passed for more than one
+			// of them.
+			LOGGER.warn("Exception occurred when trying to save the user, assuming duplicate email", e);
+			bindingResult.reject("email.exists", "Email already exists");
+			return "redirect:/signup";
+		}
+		// ok, redirect
+		return "redirect:/login";
+	}
 
 }
